e06c3af3101faa812f68a07661bccada97df759b
Gaelle <gaelle013@hotmail.com>
2026-09-08 14:00:47 +0300

    added mock data to see how chat looks like

--- lib/features/chatbot/screens/Chat_screen.dart
+++ lib/features/chatbot/screens/Chat_screen.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
 import '../../../app/theme/app_colors.dart';
 import '../widgets/chat_header.dart';
+import '../widgets/chat_message.dart';
+import '../widgets/chat_message_model.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  final List<ChatMessageModel> messages = [
+    const ChatMessageModel(
+      text: "Hi, I'm Zimi, your food delivery assistant.",
+      type: MessageType.bot,
+      time: '2:22 PM',
+    ),
+
+    const ChatMessageModel(
+      text: 'Where is my order right now?',
+      type: MessageType.user,
+      time: '2:24 PM',
+    ),
+
+    const ChatMessageModel(
+      text:
+          'Your order is on its way and should arrive in about 20 minutes. The driver is currently nearby.',
+      type: MessageType.bot,
+      time: '2:25 PM',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,16 +46,23 @@ class ChatScreen extends StatelessWidget {
             const ChatHeader(),
               
             // Chat content will go here
-            const Expanded(
-              child: Center(
-                child: Text(
-                  'Hello',
-                  style: TextStyle(
-                    color: AppColors.secondaryText,
-                    fontSize : 16,
-                  )
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.only(
+                  top: 18,
+                  bottom:20,
                 ),
-              ),
+                
+                itemCount:messages.length,
+
+                itemBuilder: (context,index),
+                  return ChatMessage(
+                    message:messages[index],
+                  )
+                 
+                ),
+            ),
+          ],
             ),
 
             // Input bar will go here
@@ -34,9 +70,9 @@ class ChatScreen extends StatelessWidget {
               height: 20,
               color: Colors.white,
             ),
-          ],
+    
         ),
       ),
-    );
+    
   }
 }
