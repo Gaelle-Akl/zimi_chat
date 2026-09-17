303703d2171fb10b4c1a8065276bbcea0efc2335
Gaelle <gaelle013@hotmail.com>
2026-09-17 10:02:43 +0300

    when pressed this should give the user the option to click on an option(individual options)

---
+++ lib/features/chatbot/widgets/quick_action_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../../../app/theme/app_colors.dart';
+
+class QuickActionButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onPressed;
+
+  const QuickActionButton({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 48,
+      child: OutlinedButton.icon(
+        onPressed: onPressed,
+        icon: Icon(
+          icon,
+          size: 19,
+          color: AppColors.primary,
+        ),
+        label: Text(
+          label,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            color: AppColors.text,
+            fontSize: 12,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        style: OutlinedButton.styleFrom(
+          backgroundColor: AppColors.white,
+          side: const BorderSide(
+            color: AppColors.border,
+            width: 1,
+          ),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+      ),
+    );
+  }
+}
